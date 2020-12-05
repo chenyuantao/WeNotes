30e25ffc660a48106086ec9c1715dab0071e377b
chenyuantao <cyt528300@gmail.com>
2020-12-05 15:58:37 +0800

    Add header info

--- src/pages/edit/components/header.tsx
+++ src/pages/edit/components/header.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from './styles.less';
 import { history } from 'umi';
 import { Note } from '@/services/notes';
+import { Popup } from 'react-weui';
+import { useState } from 'react';
+import dayjs from 'dayjs';
 
 export default ({
   note,
@@ -9,13 +12,27 @@ export default ({
 }: {
   note: Note;
   onChange: (newNote: Note) => void;
-}) => (
-  <div className={styles.header}>
-    <h3 className={styles.title}>{note.title}</h3>
-    <img
-      className={styles.back}
-      src={require('@/asserts/ic_back.png')}
-      onClick={() => history.goBack()}
-    />
-  </div>
-);
+}) => {
+  const [show, setShow] = useState(false);
+  return (
+    <div className={styles.header}>
+      <h3 className={styles.title}>{note.title}</h3>
+      <img
+        className={styles.back}
+        src={require('@/asserts/ic_back.png')}
+        onClick={() => history.goBack()}
+      />
+      <img
+        className={styles.info}
+        src={require('@/asserts/ic_info.png')}
+        onClick={() => setShow(true)}
+      />
+      <Popup show={show} onRequestClose={() => setShow(false)}>
+        <>
+          <div>{note.title}</div>
+          <div>{dayjs(note.createdAt).format('YYYY-MM-DD HH:mm:ss')}</div>
+        </>
+      </Popup>
+    </div>
+  );
+};
