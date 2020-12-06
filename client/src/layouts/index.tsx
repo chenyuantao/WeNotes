d7c9928f9ac33ac43df469679e612de5edb8304c
chenyuantao <cyt528300@gmail.com>
2020-12-06 21:32:31 +0800

    Add socket.io-client to client

--- client/src/layouts/index.tsx
+++ client/src/layouts/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { withRouter } from 'umi';
 import styles from './index.less';
+import io from 'socket.io-client';
+import { Msg } from 'react-weui';
 import 'weui';
 import 'react-weui/build/packages/react-weui.css';
 
@@ -12,6 +14,22 @@ const ANIMATION_MAP = {
 };
 
 export default withRouter(({ location, children, history }) => {
+  const [connected, setConnected] = useState(false);
+  useEffect(() => {
+    const socket = io('http://localhost:3004');
+    socket.on('connect', () => {
+      setConnected(true);
+      console.log('connect');
+    });
+    socket.on('disconnect', () => {
+      setConnected(false);
+      socket.open();
+    });
+    socket.open();
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
   return (
     <TransitionGroup
       childFactory={child =>
@@ -19,7 +37,17 @@ export default withRouter(({ location, children, history }) => {
       }
     >
       <CSSTransition key={location.pathname} timeout={200}>
-        <div className={styles.container}>{children}</div>
+        <div className={styles.container}>
+          {connected ? (
+            children
+          ) : (
+            <Msg
+              className={styles.msg}
+              type="loading"
+              title="Connecting to server..."
+            />
+          )}
+        </div>
       </CSSTransition>
     </TransitionGroup>
   );
