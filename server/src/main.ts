c0a6b4dce6954fbd14145e26396f00a9427c0e93
chenyuantao <cyt528300@gmail.com>
2020-12-07 17:45:38 +0800

    Add success interceptor to format response

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './adapters/redis.adapter';
+import { SuccessInterceptor } from './middlewares/success.interceptor';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useWebSocketAdapter(new RedisIoAdapter(app));
+  app.useGlobalInterceptors(new SuccessInterceptor());
   const port = parseInt(process.env.SERVER_PORT);
   await app.listen(port);
   console.log(`server is running on http://localhost:${port}`);
